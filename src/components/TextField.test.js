ab64dc56a6afc9866bb4c0466dd13ff0af696b11
Minh Le <minhle@Minhs-iMac.local>
2020-04-08 12:16:51 +0700

    format code

--- src/components/TextField.test.js
+++ src/components/TextField.test.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import TextField from '@/components/TextField';
+/* eslint-disable no-undef */
+import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
-import {shallow, configure} from 'enzyme';
-import { Form, Formik } from 'formik';
+import { Formik } from 'formik';
+import { shallow, configure } from 'enzyme';
+
+import TextField from '@/components/TextField';
+
 configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  shallow(<Formik>
-          <TextField label="Full name" type="text" name="name" />
-        </Formik>
-        );
+  shallow(
+    <Formik>
+      <TextField label="Full name" type="text" name="name" />
+    </Formik>,
+  );
 });
--- src/components/TimePicker.test.js
+++ src/components/TimePicker.test.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import TimePicker from '@/components/TimePicker';
+/* eslint-disable no-undef */
+import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
-import {shallow, configure} from 'enzyme';
-import {TIME_FORMAT } from '@/constants/defaultValues';
+import { shallow, configure } from 'enzyme';
+
+import { TIME_FORMAT } from '@/constants/defaultValues';
+import TimePicker from '@/components/TimePicker';
+
 configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  shallow(<TimePicker
-    time="00:00"
-    onChangeTime={()=>{}}
-    timeFormat={TIME_FORMAT}
-  />);
+  shallow(<TimePicker time="00:00" onChangeTime={() => {}} timeFormat={TIME_FORMAT} />);
 });
